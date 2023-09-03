227001f43d45ecd9c9acf6fa389c1547a6019b24
Arsalan Ahmed <arskhatri2008@gmail.com>
2023-09-03 12:39:04 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Mode = () => {
+    const [isLit, setLit] = useState(true)
+  return(
+    <div className={`room ${isLit?"Light":"Dark"}`}>
+        <p>React Modes</p>
+        <p>The page is {isLit ? "Light" : "Dark"}</p>
+        <button onClick={()=> setLit(!isLit)}>{isLit ? "Dark" : "Light"}</button>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Mode />,document.querySelector('#root'))
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
